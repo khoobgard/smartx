7eafbcf03bf39a873a87489939d08f5fcfc3a924
khoobgard <majidhassany@gmail.com>
2020-05-19 02:26:33 +0430

    admin

--- register/urls.py
+++ register/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from register.views import DeviceView
 
 urlpatterns = [
-    url
+    url(r'^admin/', admin.site.urls),
+    url(r'^devices/', DeviceView.as_view()),
 
 ]
