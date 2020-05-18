a36d7ae94e773b0291c745a628fa399787522421
khoobgard <majidhassany@gmail.com>
2020-05-19 02:14:57 +0430

    create model

--- register/models.py
+++ register/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Master(models.Model):
+    name = models.CharField(max_length=200)
+    type = models.Choices({'r':'raspberry','a':'arduino'})
+    code = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Device(models.Model):
+    name = models.CharField(max_length=200)
+    type = models.Choices()
+    code = models.IntegerField(unique=True)
+    master = models.ForeignKey(Master,on_delete=models.CASCADE)
+
+    def __str__(self):
+        
