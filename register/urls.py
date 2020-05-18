6ff6b6c1a9e778ba4e1eda084828d9f4486b64d6
khoobgard <majidhassany@gmail.com>
2020-05-19 02:39:13 +0430

    make migrate

--- register/urls.py
+++ register/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from register.views import DeviceView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^devices/', DeviceView.as_view()),
 
 ]
--- register/views.py
+++ register/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from register.models import Device,Master
-from django.views.generic import ListView,DetailViews
+from django.views.generic.list import ListView
 # Create your views here.
- class DeviceView(ListView):
+class DeviceView(ListView):
      model = Device
 
      def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
+
         return context
