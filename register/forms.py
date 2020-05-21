f52769bb111a3d748d4be2168c07ab0347664ca8
khoobgard <majidhassany@gmail.com>
2020-05-22 02:47:12 +0430

    .

--- register/forms.py
+++ register/forms.py
@@ -4,3 +4,4 @@ class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required = False , widget=forms.HiddenInput)
