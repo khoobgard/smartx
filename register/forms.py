dd36b8c5d63378b252a907c6cb34fdf758b2fa89
khoobgard <majidhassany@gmail.com>
2020-05-29 18:26:40 +0430

    .

--- register/forms.py
+++ register/forms.py
@@ -2,19 +2,19 @@ from django import forms
 from django.contrib.auth.models import User
 from register.models import UserProfileInfo
 from register.models import Device,Master
-from captcha.fields import ReCaptchaField
-from captcha.widgets import ReCaptchaV2Invisible
+# from captcha.fields import ReCaptchaField
+# from captcha.widgets import ReCaptchaV2Invisible
 
 
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
-    captcha = ReCaptchaField(
-    public_key='6Ldou_0UAAAAANymj1RdQ7VqZy97FLslCAI02aCU',
-    private_key='6Ldou_0UAAAAAAFPVYs7nl3FEL5op63LtTm-W3y_',
-)
-    captcha = ReCaptchaField(widget=ReCaptchaV2Invisible)
+#     captcha = ReCaptchaField(
+#     public_key='6Ldou_0UAAAAANymj1RdQ7VqZy97FLslCAI02aCU',
+#     private_key='6Ldou_0UAAAAAAFPVYs7nl3FEL5op63LtTm-W3y_',
+# )
+#     captcha = ReCaptchaField(widget=ReCaptchaV2Invisible)
 
     class Meta():
         model = User
