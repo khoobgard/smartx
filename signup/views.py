76380819bb0e44b12cd65e6142c65a9be3d283d7
khoobgard <majidhassany@gmail.com>
2020-06-11 19:23:52 +0430

    .

--- signup/views.py
+++ signup/views.py
@@ -10,7 +10,6 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request,'signup/index.html')
 
-
 @login_required
 def user_logout(request):
     logout(request)
--- vehicles/views.py
+++ vehicles/views.py
@@ -6,6 +6,7 @@ from vehicles import forms
 from django.http import HttpResponseRedirect , HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from signup.views import index
 
 class VehicleListView(ListView):
     context_object_name = 'vehicles'
